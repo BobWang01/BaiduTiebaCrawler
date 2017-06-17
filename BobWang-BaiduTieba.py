e23e343ab841111c2aa52a4acabd668ef8e8e1f0
BobWong <463550934@qq.com>
2017-06-17 22:49:59 +0800

    Initial commit

---
+++ BobWang-BaiduTieba.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2
+#-*- coding: utf-8 -*-：
+import re
+import urllib
+import os
+
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+
+def getImg(html, x):
+    localPath='image'
+    os.mkdir(localPath)
+    reg = r'src="(http://img.+?\.jpg)"'
+    imgre = re.compile(reg)
+    imList = re.findall(reg, html)
+
+    print(imList)
+    for i in imList:
+        print(i)
+        print x
+        urllib.urlretrieve(i, localPath+'/%d.jpg' % x)
+        x += 1
+    return x
+print 'Choose 1 page enter one (one)'
+print 'Choose more pages enter more (more)'
+Choose = raw_input('Do you want to scapy only one page or more pages? ')
+try:
+
+    if Choose == 'one':
+        x = 1
+        url1=raw_input('Please enter url of this one page: ')
+        url = url1
+        html = getHtml(url)
+        x = getImg(html, x)
+
+    elif Choose == 'more':
+        x = 1
+        url = raw_input('Enter url: ')
+        for k in range(1, 10):
+            ul = url + str(k)
+            print ul
+            html = getHtml(ul)
+
+            x = getImg(html, x)
+
+except:
+    print'Enter one or more'
